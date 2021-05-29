fe5477fff3212934454eb4b89a63180ce96384e0
VladimirNikl <martinukvn@ukr.net>
2021-05-30 00:08:59 +0300

    111. Zadanie2

---
+++ FindAndReplaceManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    static class FindAndReplaceManager   // нельзя объявлять индексаторы в статическом классе.
+    {
+        public static string[] book;
+        public static void FindNext(string str)
+        {
+            for(int i=0; i<book.Length; i++)
+            {
+                if (book[i] == str)
+                {
+                    Console.WriteLine("'"+str+"' -" + "  является номером " + i + " в книге .");
+                    return;  // return - выход из метода и Console.WriteLine("Нет такого слова в книге.") - не напечатается.
+                } 
+            }
+            Console.WriteLine("Нет такого слова в книге.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main()
+        {
+            FindAndReplaceManager.book =  new[] { "dsfgbdf" , "rtewt" , "" , "treth" , "rthfh" , "thwrthf" , "bbb"};
+            Console.WriteLine(" Введите слово для поиска");
+            FindAndReplaceManager.FindNext(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
+     
