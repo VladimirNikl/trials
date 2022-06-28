ec2d0e9a3cc51133294b8b9fa82d694504c518ec
VladimirNikl <martinukvn@ukr.net>
2022-06-28 18:35:41 +0300

    method master

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ namespace Zadanie2
 {
     class Program
     {
+       static void Method()
+        {
+            Console.WriteLine(" Branch 'master'");
+        }
         static void Main()
         {
             FindAndReplaceManager.book =  new[] { "dsfgbdf" , "rtewt" , "" , "treth" , "rthfh" , "thwrthf" , "bbb"};
             Console.WriteLine(" Введите слово для поиска");
             FindAndReplaceManager.FindNext(Console.ReadLine());
+
+            Method();
 
             Console.ReadKey();
         }
