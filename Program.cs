61b72c1ed88ccf5e56311b256c0e28266c5150c3
VladimirNikl <martinukvn@ukr.net>
2022-06-28 19:25:32 +0300

    further

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ namespace Zadanie2
 {
     class Program
     {
+        static void method()
+        {
+            Console.WriteLine(" method master");
+        }
         static void Main()
         {
             FindAndReplaceManager.book =  new[] { "dsfgbdf" , "rtewt" , "" , "treth" , "rthfh" , "thwrthf" , "bbb"};
             Console.WriteLine(" Введите слово для поиска");
             FindAndReplaceManager.FindNext(Console.ReadLine());
+
+            method();
 
             Console.ReadKey();
         }
